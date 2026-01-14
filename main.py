0db0b2c28a6e112ab15e0503600c37f53d2de453
hsyhhssyy <hsyhhssyy@gmail.com>
2026-01-14 09:22:21 +0000

    store

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ if __name__ == "__main__":
     from .src.entrypoints.uvicorn_host import uvicorn_main
     uvicorn_main()
 else:
-    from .src.adapters.astrbot.plugin import MyPlugin as RealPlugin
+    from .src.adapters.astrbot.plugin import MyPlugin
