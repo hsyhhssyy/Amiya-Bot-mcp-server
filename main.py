4b592ce05d6e607159314b93c7eaeaf8ae499cb0
hsyhhssyy <hsyhhssyy@gmail.com>
2026-01-14 05:01:21 +0000

    store

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger,AstrBotConfig
+from astrbot.api import logger
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
-    def __init__(self, context: Context, config: AstrBotConfig):
+    def __init__(self, context: Context):
         super().__init__(context)
         
-        self._astrbot_config = config
+        # self._astrbot_config = config
         # self.ctx: AppContext | None = None
         
 
