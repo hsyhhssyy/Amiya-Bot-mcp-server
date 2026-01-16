04dbedc1ca648855efedb40c028f7fbcdceb847c
hsyhhssyy <hsyhhssyy@gmail.com>
2026-01-16 09:45:08 +0000

    store

--- main.py
+++ main.py
@@ -2,77 +2,18 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger,AstrBotConfig # 使用 astrbot 提供的 logger 接口
 
-# from .src.app.bootstrap_astrbot import build_context_from_astrbot
-# from .src.app.context import AppContext
-
-# from .src.helpers.gamedata.search import search_source_spec, build_sources
-
-# from .src.domain.services.operator import search_operator_by_name
-# from .src.domain.models.operator import Operator
-
-class AmiyaBotAstrbotPlugin(Star):
-    def __init__(self, context: Context):
+class MyPlugin(Star):
+    def __init__(self, context: Context,config: AstrBotConfig):
         super().__init__(context)
-        
-        # self._astrbot_config = config
-        # self.ctx: AppContext | None = None
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-        pass
-    #     self.ctx = await build_context_from_astrbot(self._astrbot_config)
-    #     print("AmiyaBotAstrbotPlugin resource root:", self.ctx.cfg.GameDataPath)
-    
-    # @filter.command("查询")
-    # async def operator_archives_operator_query(self, event: AstrMessageEvent):
-
-    #     if not self.ctx or not self.ctx.cfg.ProjectRoot:
-    #         yield event.plain_result("❌ 插件未初始化完成，请稍后再试。")
-    #         return
-
-    #     query_str = event.message_str.strip()
-
-    #     try:
-    #         logger.info(f"查询干员: {query_str}")
-            
-    #         search_sources = build_sources(self.ctx.data_repository.get_bundle(), source_key=["name"])
-    #         search_results = search_source_spec(
-    #             query_str,
-    #             sources=search_sources
-    #         )
-
-    #         if not search_results:
-    #             yield event.plain_result("未找到干员!")
-    #             return
-    #         elif len(search_results.matches) > 1:
-    #             # 交互式选择结果
-    #             matched_names = [m.matched_text for m in search_results.matches if m.key == "name"]
-    #             yield event.plain_result(
-    #                 f"❌ 找到多个匹配的干员名称: {', '.join(matched_names)}，请提供更精确的名称。"
-    #             )
-    #             return
-            
-    #         op: Operator = search_results.by_key("name")[0].value
-
-    #         result = search_operator_by_name(self.ctx, op.name)
-
-    #         # 从磁盘读取template
-    #         template_file = self.ctx.cfg.ProjectRoot / "data" / "templates" / "html" / "operator_info.html"
-
-    #         if not template_file.exists():
-    #             yield event.plain_result(f"模板文件{template_file}不存在，无法渲染结果!")
-    #             return
-
-    #         TMPL = template_file.read_text(encoding="utf-8")
-
-    #         url = await self.html_render(TMPL, result.data, options={}) # 第二个参数是 Jinja2 的渲染数据
-    #         yield event.image_result(url)
-    #         return
-    #     except Exception as e:
-    #         logger.exception("查询干员信息失败")
-    #         yield event.plain_result("未找到干员!")
-    #         return
+    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
+    @filter.command("helloworld")
+    async def helloworld(self, event: AstrMessageEvent):
+        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
+        user_name = event.get_sender_name()
+        message_str = event.message_str # 获取消息的纯文本内容
+        logger.info("触发hello world指令!")
+        yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
 
     async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
-        pass
+        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
