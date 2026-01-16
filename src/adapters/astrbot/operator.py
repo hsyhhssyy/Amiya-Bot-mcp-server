cb7ddb046076eddc0cdaf03456b5a8efa540272f
hsyhhssyy <hsyhhssyy@gmail.com>
2026-01-16 03:42:48 +0000

    store

--- src/adapters/astrbot/operator.py
+++ src/adapters/astrbot/operator.py
@@ -1,3 +1,10 @@
+from astrbot.api.event import filter, AstrMessageEvent
 
 class OperatorQueryMixin:
-    pass    
+    @filter.command("干员查询")
+    async def operator_archives_operator_query(self, event: AstrMessageEvent):
+        user_name = event.get_sender_name()
+        message_str = event.message_str
+        yield event.plain_result(
+            f"Hello, {user_name}, 你发了 {message_str}!"
+        )
