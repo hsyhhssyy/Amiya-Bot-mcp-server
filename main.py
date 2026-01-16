182797fd47f92ad76758008a1c6230ece6563aab
hsyhhssyy <hsyhhssyy@gmail.com>
2026-01-16 09:46:45 +0000

    store

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger,AstrBotConfig # 使用 astrbot 提供的 logger 接口
+from astrbot.api import logger,AstrBotConfig
+
+from .src.app.context import AppContext
 
 class MyPlugin(Star):
     def __init__(self, context: Context,config: AstrBotConfig):
         super().__init__(context)
-
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 获取消息的纯文本内容
-        logger.info("触发hello world指令!")
-        yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
+        self._astrbot_config = config
+        self.ctx: AppContext | None = None
 
     async def terminate(self):
         '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
